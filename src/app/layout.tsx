129ddef7af62623081144b68bbd63d6b6d4b2326
Alejandro Peralta <agperalta@gmail.com>
2026-04-06 12:45:51 +0200

    more manifest.json fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
 
 
         <head>
-            <link rel="manifest" href="/manifest.json"/>
+            <link rel="manifest" href="/manifest.json?v=3"/>
             <meta name="apple-mobile-web-app-capable" content="yes"/>
             <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
             <meta name="theme-color" content="#228be6"/>
